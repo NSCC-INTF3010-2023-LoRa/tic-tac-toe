2797ea47b475708c488f06d6d4332d6e3b9d090b
Chris Nasser <W0481966@campus.nscc.ca>
2023-03-22 22:54:29 -0300

    Made the grid look a bit nicer.

--- tic-tac-toe.ino
+++ tic-tac-toe.ino
@@ -1,9 +1,9 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
 
-#define TFT_CS   4 /* shield pin 10 */
-#define TFT_DC   3 /* shield pin 9  */
-#define TFT_SCK  7 /* shield pin 13 */
+#define TFT_CS 4   /* shield pin 10 */
+#define TFT_DC 3   /* shield pin 9  */
+#define TFT_SCK 7  /* shield pin 13 */
 #define TFT_MISO 6 /* shield pin 12 */
 #define TFT_MOSI 5 /* shield pin 11 */
 // -1 for the reset pin, which we don't use
@@ -11,15 +11,19 @@ Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCK, -1, T
 
 void setup() {
   tft.begin();
-  tft.fillScreen(ILI9341_BLACK);
+  tft.fillScreen(ILI9341_WHITE);
   tft.setTextColor(ILI9341_BLUE, ILI9341_BLACK);
   tft.setTextSize(2);
   tft.setTextWrap(false);
 
-  tft.drawLine(80, 40, 80, 280, ILI9341_WHITE);
-  tft.drawLine(160, 40, 160, 280, ILI9341_WHITE);
-  tft.drawLine(0, 120, 240, 120, ILI9341_WHITE);
-  tft.drawLine(0, 200, 240, 200, ILI9341_WHITE);
+  // Display the tic-tac-toe grid. We draw multiple lines to
+  // display thicker lines
+  for (int i = -1; i <= 1; i++) {
+    tft.drawLine(80 + i, 40 + i, 80 + i, 280 + i, ILI9341_BLACK);
+    tft.drawLine(160 + i, 40 + i, 160 + i, 280 + i, ILI9341_BLACK);
+    tft.drawLine(0 + i, 120 + i, 240 + i, 120 + i, ILI9341_BLACK);
+    tft.drawLine(0 + i, 200 + i, 240 + i, 200 + i, ILI9341_BLACK);
+  }
 }
 
 void loop() {
